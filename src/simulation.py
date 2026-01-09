e91488bf3bfa465d995daa3b9b7512b87de9352f
DonVato97 <leo.m321@gmx.de>
2026-01-09 22:58:49 +0100

    update

--- src/simulation.py
+++ src/simulation.py
@@ -1,19 +1,13 @@
-from Board import create_board, make_move, check_win
+from board import create_board, make_move, check_win
 
-def simulate_game(player1_func, player2_func,
-                  moves_count_p1=None, moves_count_p2=None,
-                  wins_count_p1=None, wins_count_p2=None):
-    from board import create_board, make_move, check_win
-
-    def simulate_game(player1_func, player2_func):
-        board = create_board()
-        turn = 1
-        while True:
-            move = player1_func(board, 1) if turn == 1 else player2_func(board, 2)
-            make_move(board, move, turn)
-            if check_win(board, turn):
-                return turn
-            if len([c for c in range(7) if board[0, c] == 0]) == 0:
-                return 0
-            turn = 3 - turn
-
+def simulate_game(player1_func, player2_func):
+    board = create_board()
+    turn = 1
+    while True:
+        move = player1_func(board, 1) if turn == 1 else player2_func(board, 2)
+        make_move(board, move, turn)
+        if check_win(board, turn):
+            return turn
+        if len([c for c in range(7) if board[0, c]==0]) == 0:
+            return 0
+        turn = 3 - turn
